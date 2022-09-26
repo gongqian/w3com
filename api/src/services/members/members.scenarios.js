4ecf757d6700639d9af66af0246f1c43a339ad03
gongqian <gongqian@gmail.com>
2022-09-26 00:26:09 -0400

    delete service files

--- api/src/services/members/members.scenarios.js
+++
@@ -1,6 +0,0 @@
-export const standard = defineScenario({
-  member: {
-    one: { data: { openId: 'String8434518' } },
-    two: { data: { openId: 'String7852495' } },
-  },
-})
--- api/src/services/profiles/profiles.scenarios.js
+++
@@ -1,6 +0,0 @@
-export const standard = defineScenario({
-  profile: {
-    one: { data: { member: { create: { openId: 'String8627581' } } } },
-    two: { data: { member: { create: { openId: 'String1163572' } } } },
-  },
-})
